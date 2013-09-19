ec5fb7bec94bcd2190afbeaaeb93f2697d52208b
cerdmann <erds@badmoondev.com>
2013-09-19 15:38:48 -0500

    added nano files

--- copydotfiles.sh
+++ copydotfiles.sh
@@ -9,7 +9,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bashrc bash_profile"       # list of files/folders to symlink in homedir
+files="bashrc bash_profile nanorc"       # list of files/folders to symlink in homedir
 
 ##########
 
