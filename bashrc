54f8f757eeed3eb8b3d23b8554b5cdc22a991ba2
cerdmann <erds@badmoondev.com>
2014-03-22 21:11:36 -0500

    Added vimrc

--- bashrc
+++ bashrc
@@ -41,3 +41,4 @@ set -o noclobber
 
 export GREP_OPTIONS='--color=auto'
 export GREP_COLOR='1;31'
+export VAGRANT_DEFAULT_PROVIDER=parallels
--- copydotfiles.sh
+++ copydotfiles.sh
@@ -9,7 +9,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bashrc bash_profile nanorc"       # list of files/folders to symlink in homedir
+files="bashrc bash_profile nanorc vimrc"       # list of files/folders to symlink in homedir
 
 ##########
 
